145a8d74a5f178399e4663a4b2dd3d0f5780c2cd
GLun <areco.eduardo@gmail.com>
2017-11-26 11:47:26 -0300

    modificaciones de rutas para habitaciones

--- hostelapp/routes/index.js
+++ hostelapp/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Casa AltApp' });
 });
 
+/* Muestra pagina de habitaciones*/
+router.get('/habitaciones', function(req, res, next) {
+  res.render('index', { title: 'Casa AltApp' });
+});
+
 module.exports = router;
